a64dec32dedde41c0acc5d42387861d471a6e8f2
Hannah Karlsson <gamemod.mod@gmail.com>
2022-09-12 13:17:03 +0200

    Add project files.

---
+++ OOPDemo/Player.cs
@@ -0,0 +1,28 @@
+﻿namespace OOPCar
+{
+    public class Car
+    {
+        private string _make;
+        private string _color;
+
+        public string Make
+        {
+            get { return _make.ToUpper(); }
+            set { _make = value; }
+        }
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value.ToUpper(); }
+        }
+        public bool Debug { get; set; }
+        public void Print(int id)
+        {
+            Console.WriteLine($"Bil #{id} är en {Make} och har färgen {Color}");
+            if (!Debug) return;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Make = {Make}, _make = {_make}, Color = {Color}, _color = {_color}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ OOPDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using OOPCar;
+
+var car = new Car
+{
+    Make = "Volvo",
+    Color = "Röd",
+    Debug = true
+};
+var car2 = new Car
+{
+    Make = "Citroen",
+    Color = "Svart"
+};
+Car[] cars = { car, car2 };
+for (var i = 0; i < cars.Length; i++)
+{
+    cars[i].Print(i + 1);
+}
