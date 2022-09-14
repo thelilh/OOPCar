694c3bf8eb4f2b61374db3fcd435d1954293d86a
Hannah Karlsson <gamemod.mod@gmail.com>
2022-09-14 15:04:16 +0200

    Car List

--- OOPDemo/Player.cs
+++ OOPDemo/Player.cs
@@ -3,6 +3,7 @@
     public class Car
     {
         private string _make;
+        public int _id;
         private string _color;
 
         public string Make
@@ -16,13 +17,40 @@
             set { _color = value.ToUpper(); }
         }
         public bool Debug { get; set; }
-        public void Print(int id)
+        public void Print()
         {
-            Console.WriteLine($"Bil #{id} är en {Make} och har färgen {Color}");
+            switch (Color.ToLower())
+            {
+                case "röd":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "svart":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "blå":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "grön":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "vit":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                default:
+                    Console.WriteLine(":(");
+                    break;
+            }
+            Console.WriteLine($"Bil #{_id} är en {Make} och har färgen {Color}");
+            Console.ResetColor();
             if (!Debug) return;
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Make = {Make}, _make = {_make}, Color = {Color}, _color = {_color}");
+            Console.WriteLine(ToString());
             Console.ResetColor();
+        }
+
+        public override string ToString()
+        {
+            return $"Make: {Make}, _make: {_make}, Color: {Color}, _color: {_color}";
         }
     }
 }
--- OOPDemo/Program.cs
+++ OOPDemo/Program.cs
@@ -1,18 +1,58 @@
 ﻿using OOPCar;
 
-var car = new Car
+string[] makes =
 {
-    Make = "Volvo",
-    Color = "Röd",
-    Debug = true
+    "Volvo v70",
+    "Volvo c30",
+    "Volvo XC60",
+    "Volvo XC90",
+    "Volvo XC40",
+    "BMW x5",
+    "BMW x1",
+    "BMW z4",
+    "BMW 7-serie",
+    "BMW 3-serie",
+    "Audi A1",
+    "Audi A3",
+    "Audi A4",
+    "Audi A5",
+    "Audi A6",
+    "Audi A7",
+    "Audi A8",
+    "SAAB 95",
+    "SAAB 93",
+    "Seat Arona",
+    "Seat Ateca",
+    "Seat Ibiza",
+    "Seat Léon",
+    "Seat Tarraco",
+    "Polestar 1",
+    "Polestar 2"
 };
-var car2 = new Car
+string[] colors =
 {
-    Make = "Citroen",
-    Color = "Svart"
+    "Röd",
+    "Svart",
+    "Blå",
+    "Grön",
+    "Vit"
 };
-Car[] cars = { car, car2 };
-for (var i = 0; i < cars.Length; i++)
+
+var carList = new Stack<Car> { };
+for (var i = 100; i > 0; i--)
 {
-    cars[i].Print(i + 1);
+    var rand = new Random();
+    var car = new Car
+    {
+        _id = i,
+        Color = colors[rand.Next(0, colors.Length)],
+        Debug = false,
+        Make = makes[rand.Next(0, makes.Length)]
+    };
+    carList.Push(car);
+}
+
+foreach (var x in carList)
+{
+    x.Print();
 }
